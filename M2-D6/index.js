4c1f8f623035eea11d87e8944d7215e84fe0d9de
BusayoAfolabi12 <97314339+BusayoAfolabi12@users.noreply.github.com>
2022-07-11 17:57:47 +0200

    Module2-day6 exercise

--- M2-D6/index.js
+++ M2-D6/index.js
@@ -1,4 +1,3 @@
-
 //.....TO CHANGE THE HEADER innerHtml.....//
 
 const changeHeaderText = document.querySelector("h1.mainheader");
@@ -9,28 +8,34 @@ changeHeaderText.innerHTML = "Unique Smartphone Shop";
 // changeHeaderText.innerHTML = "Unique Smartphone Shop";
 
 function randomNumber(x) {
-let randomNumber = Math.floor(Math.random() * x)
-return randomNumber 
- }
+  let randomNumber = Math.floor(Math.random() * x);
+  return randomNumber;
+}
 
-//....OPEN THIS TO CHANGE HEADER_TEXT COLOR....// 
+//....OPEN BELOW TO CHANGE HEADER_TEXT COLOR....//
 // let red = randomNumber(250)
 // let green = randomNumber(100)
 // let blue = randomNumber(110)
 // mainheader.style.color = `rgb(${red}, ${green}, ${blue})`
 
-
 //......TO CHANGE THE BACKGROUND COLOR......//
-const html = document.querySelector("html")
+const body = document.querySelector("body");
 
-let red2 = randomNumber(255)
-let green2 = randomNumber(255)
-let blue2 = randomNumber(255)
-html.style.backgroundColor = `rgb(${red2}, ${green2}, ${blue2})`;
+let red2 = randomNumber(255);
+let green2 = randomNumber(255);
+let blue2 = randomNumber(255);
+body.style.backgroundColor = `rgb(${red2}, ${green2}, ${blue2})`;
 
 const changeAddress = document.querySelector("p.address");
-changeAddress.innerHTML = "Am Dobben str. 2 23765, Hamburg. This is a fake address";
+changeAddress.innerHTML =
+  "Am Dobben str. 2 23765, Hamburg. This is a fake address";
+
+  
+const phoneLinks = document.querySelectorAll("a.amazon_links")
 
 
-
+for (let i = 0; i < phoneLinks.length; i++){
+    let linkColor = phoneLinks[i]
+    linkColor.style.color = "red";
+}
 
