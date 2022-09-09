2382bbda225155ae7dbba9a83c247fb5b4656dba
BusayoAfolabi12 <97314339+BusayoAfolabi12@users.noreply.github.com>
2022-09-09 20:38:16 +0200

    Update index.js

--- M4-Day1 Exercise/index.js
+++ M4-Day1 Exercise/index.js
@@ -141,7 +141,7 @@ function largestEven(arr) {
 function signChecker(a, b) {
   return (a < 0 && b > 0) || (a > 0 && b < 0);
 }
-console.log(signChecker(-35, -6));
+console.log(signChecker(-25, -6));
 
 /*16. Create a function to create and return a new string where the first 3 characters are in lower case and the others are in upper case. If the string’s length is less than 3, convert the whole string into uppercase. Pass the original string as a parameter.
 17. Create a function to calculate the sum of two integers (passed as parameters). If the sum is in the 50-80 range, return 65, otherwise, return 80.*/
